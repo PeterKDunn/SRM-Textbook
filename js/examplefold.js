8714fef680dab282a82df4d08d78e750c855c16c
Peter Dunn <pdunn2@usc.edu.au>
2022-06-08 12:32:52 +1000

    Change exampleExtra to "Example" label

--- js/examplefold.js
+++ js/examplefold.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
   $examplefolds = $(".exampleExtra");
   $examplefolds.wrapInner("<div class=\"example-fold-blck\">"); // wrap a div container around content
-  $examplefolds.prepend("<button class=\"example-fold-btn\" style=\"border-style=solid; border-color: black; padding: 12px 25px; font-size: larger; background-color=LightSlateGrey\">Extra example: Show</button>");  // add a button
+  $examplefolds.prepend("<button class=\"example-fold-btn\" style=\"border-style=solid; border-color: black; padding: 12px 25px; font-size: larger; background-color=LightSlateGrey\">Example: Show</button>");  // add a button
   $(".example-fold-blck").toggle();  // fold all blocks
   $(".example-fold-btn").on("click", function() {  // add onClick event
-    $(this).text($(this).text() === "Extra example: Hide" ? "Extra example: Show" : "Extra example: Hide");  // if the text equals "Extra example: Hide", change it to "Extra example: Show"or else to "Extra example: Hide" 
+    $(this).text($(this).text() === "Example: Hide" ? "Example: Show" : "Example: Hide");  // if the text equals "Example: Hide", change it to "Example: Show"or else to "Example: Hide" 
     $(this).next(".example-fold-blck").toggle("linear");  // "swing" is the default easing function. This can be further customized in its speed or the overall animation itself.
   })
 });
