36058c8260ccbc0b6c08a8d2a16e19649f2757f4
Peter K Dunn <pdunn2@usc.edu.au>
2023-01-05 15:32:36 +1000

    add shadeNormal(), plotNormal()

--- R/NormalDistributions.R
+++ R/NormalDistributions.R
@@ -1,3 +1,252 @@
+shadeNormal <- function(xx, yy, lo, hi, col="blue"){
+  
+  xshade <- xx[ (xx >= lo) & ( xx <= hi ) ]
+  yshade <- yy[ (xx >= lo) & ( xx <= hi ) ]
+  y0 <- rep(0, length(yshade))
+  
+  polygon( x = c(xshade, rev(xshade) ),
+           y = c( yshade, y0),
+           col = col)
+}
+
+plotNormal <- function(mu, sd,
+                       showX = seq(-3, 3, by = 1) * sd + mu,
+                       round.dec = 1,
+                       main = "", # Main title
+                       xlab = "", # horizontal axis labels
+                       showZ = TRUE, # Whether to show z = -3:3 or not
+                       srt = 0,
+                       bg = "white", 
+                       cex.tickmarks = 1,
+                       las = 1,
+                       xlim.hi = mu + 3.5*sd, 
+                       xlim.lo = mu - 3.5*sd,
+                       axis.labels = NULL){
+  
+  # mu  is the mean of the distn
+  # sd  is the std dev of the distn
+  # round.dec  is the number of decimals to round to on the shown x-axis
+  #    (full precision used in calculations)
+  # las: The  las  parameter in par, for labelling horizontal axis
+  # srt: String rotation of the x-axis labels.
+  # cex.tickmarks: The value of  cex  for the tickmark labels
+  # main  is the main title to use
+
+  hor <- seq(xlim.lo, 
+             xlim.hi, 
+             length = 500)
+
+  nc <- dnorm(hor, 
+              mean = mu,
+              sd = sd)
+
+  extra <- 0.25 # extra space at ends
+
+  plot( nc ~ hor, 
+          axes = FALSE,
+          bg = bg,
+          ylim = c(0, max(nc) ),
+          xlim = c(xlim.lo, xlim.hi),
+          lwd = 2,
+          xlab = xlab,
+          ylab = "",
+          main = main,
+          type = "l")
+  axis(side = 1,
+       at = showX)
+  arrows( x0 = xlim.lo, 
+          x1 = xlim.hi,
+          y0 = 0,
+          y1 = 0,
+          length = 0.15, 
+          angle = 20, 
+          lwd = 2)
+  if (showZ)
+    abline( v = seq(-3, 3, by = 1) * sd + mu,
+            col = "grey")
+
+  
+  return( list(y = nc, 
+               x = hor))
+}
+
+
+
+
+
+##############################################################
+##############################################################
+##############################################################
+##############################################################
+
+
+
+
+
+
+plot.normZ <- function(mu, sd, xlab.name="Variable",
+                       new=TRUE,
+                       shade.lo.x=NA, shade.hi.x=NA,
+                       shade.lo.z=NA, shade.hi.z=NA,
+                       show.lo=NA, show.hi=NA,
+                       round.dec=1,
+                       shade.col=plot.colour,
+                       main="",
+                       width=6, # WAS 3.5
+                       height=width,
+                       type="z",
+                       las=1,
+                       srt=0,
+                       cex.tickmarks=1,
+                       zlim.hi = 3.5, zlim.lo=-zlim.hi,
+                       axis.labels=NULL){
+  
+  # mu  is the mean of the distn
+  # sd  is the std dev of the distn
+  # xlab.name  is the  xlab  label
+  # new is TRUE by default: a new plot is drawn.  If FALSE, the plot is added to the current device 
+  # shade.lo.z  is the lower shade limit (in terms of z, not x)
+  # shade.hi.z  is the upper shade limit (in terms of z, not x)
+  # show.lo  is a LOGICAL for showing the lower x-score 
+  #   If it is a number, that number is placed at the lo position instead
+  # show.hi  is a LOGICAL for showing the lower x-score explicitly
+  #   If it is a number, that number is placed at the lo position instead
+  # zlim.lo  /zlim.hi  is the lower (upper) limit of z on which to draw
+  # round.dec  is the number of decimals to round to on the shown x-axis
+  #    (full precision used in calculations)
+  # type  is the the type of course, generally "z" or "t", placed as a label on the horizontal axis
+  # las: The  las  parameter in par, for labelling horizontal axis
+  # shade.col  is the shading colour, defaulting to "wheat" (see ?colours)
+  # main  is the main title to use
+  # width  and  height  specify the width and height of the x11 device window
+  
+  #if ( is.na(shade.lo.z) ) {
+  #	warning(" shade.lo.z  must be given.")
+  #}
+  #if (  is.na(shade.hi.z) ) {
+  #	warning(" shade.hi.z  must be given.")
+  #}
+  
+  if ( new ) {
+    #   quartz(width=width,height=height, bg="white")
+    par(mar=c(2, 0, 2, 0) + 0.1  )
+  }
+  
+  if ( is.na(shade.lo.z) ) {
+    shade.lo.z <- (shade.lo.x - mu)/sd
+  }
+  if ( is.na(shade.hi.z) ) {
+    shade.hi.z <- (shade.hi.x - mu)/sd
+  }
+  if (is.na(shade.lo.x) ) {
+    shade.lo.x <- shade.lo.z * sd + mu
+  }
+  if (is.na(shade.hi.x) ) {
+    shade.hi.x <- shade.hi.z * sd + mu
+  }
+  
+  
+  hor <- seq(zlim.lo, zlim.hi, length=250) # z-scores
+  nc <- dnorm(hor, 0, 1) # Normal curve
+  extra <- 0.25 # extra space at ends
+  spacer <- -0.05 # space to other x-axis
+  text.loc <- c(-3, -2, -1, 0, 1, 2, 3) # In terms of z
+  
+  
+  if (new) {
+    plot( nc ~ hor, 
+          axes=FALSE,
+          ylim=c(-0.1, 0.4),
+          xlim=c(zlim.lo-2*extra , zlim.hi+2*extra),
+          lwd=2,
+          xlab="",
+          ylab="",
+          main=main,
+          type="l")
+  }
+  
+  lines( c(zlim.lo-extra, zlim.hi+extra) , c(0,0), lwd=2 )
+  #arrows(0, spacer, 3.75, spacer, length=0.15, angle=20, lwd=2)
+  
+  #lines( c(zlim.lo-extra, zlim.hi+extra) , c(spacer, spacer), lwd=2 )
+  #arrows(0, 0, 3.75, 0, length=0.15, angle=20, lwd=2)
+  
+  lines( c(0,0.4) ~ c(0, 0), lwd=2 )
+  
+  # text(zlim.hi+1.5*extra, spacer, adj=0,"z")
+  #text(zlim.hi-extra, 2*spacer, pos=1, adj=0,xlab.name)
+  title(sub=xlab.name, line=0)
+  
+  #text(zlim.hi+1.5*extra, 0, adj=0,xlab.name)
+  text(zlim.hi+extra, 0, pos=3, adj=0, "z")
+  
+  # Label horizontal axis
+  if ( is.null(axis.labels)){
+    text( text.loc, 0, 
+          pos=1, 
+          cex=cex.tickmarks,
+          srt=srt,
+          labels=as.character(text.loc) )
+  } else {
+    text( text.loc, 0, 
+          pos=1, 
+          cex=cex.tickmarks,
+          srt=srt,
+          labels=axis.labels )
+  }
+  #  text( text.loc, 0, pos=1, labels=as.character(text.loc), las=las )
+  #text( text.loc, spacer, pos=1, 
+  #   labels=as.character(round(text.loc*sd + mu, round.dec)) )
+  
+  
+  lines(c(shade.lo.z, shade.lo.z), c(0, dnorm(shade.lo.z,0,1)), lwd=2)
+  lines(c(shade.hi.z, shade.hi.z), c(0, dnorm(shade.hi.z,0,1)), lwd=2)
+  
+  if ( !is.na(show.lo) ) {
+    if (is.logical(show.lo) ){
+      if ( show.lo ) {
+        lines( c(shade.lo.z, shade.lo.z), c(0, dnorm(shade.lo.z,0,1)))
+        text(shade.lo, -0.11, as.character(shade.lo.z) )
+        #	      text(shade.lo, spacer-150, as.character(shade.lo.z) )
+      }
+    } else { # Not logical:  a value/character is supplied
+      lines( c(shade.lo.z, shade.lo.z), c(0, dnorm(shade.lo.z,0,1)))
+      #      text(shade.lo, spacer-0.05, as.character(show.lo) )
+      #      text(shade.lo, -0.11, as.character(show.lo) )
+    }
+  }
+  
+  if ( !is.na(show.hi) ) {
+    if (is.logical(show.hi) ){
+      if( show.hi ) {
+        lines( c(shade.hi.z, shade.hi.z), c(0, dnorm(shade.hi.z,0,1)))
+        #text(shade.hi.z, -0.11, as.character(shade.hi.z) )
+      }
+    } else {
+      lines( c(shade.hi.z, shade.hi.z), c(0, dnorm(shade.hi.z,0,1)))
+      #text(shade.hi.z, -0.11, as.character(show.hi) )
+    }
+  }
+  
+  # Shading
+  x.poly <- seq( max(zlim.lo, shade.lo.z), 
+                 min(zlim.hi, shade.hi.z), length=100)
+  y.poly <- dnorm( x.poly, 0, 1)
+  
+  x.p <- c( x.poly, rev(x.poly) )
+  y.p <- c( rep(0, length(x.poly)), rev(y.poly) )
+  polygon(x.p, y.p, col=shade.col )
+  
+  # Rug on horizontal axis
+  yrange <- max(nc) - 0
+  for (i in 1:length(text.loc)) {
+    lines( c(text.loc[i], text.loc[i]),
+           c(0, 0.05*yrange),
+           col="grey")
+  } 
+}
+
+
 plot.norm <- function(mu, sd, 
                       xlab.name = "Variable",
                       new = TRUE,
