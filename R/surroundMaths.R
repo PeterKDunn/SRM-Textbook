e15e01a7371c5bc44c4cfb99446d9752be80e591
Peter Dunn <pdunn2@usc.edu.au>
2023-10-18 18:17:24 +1000

    Fix errors

--- R/surroundMaths.R
+++ R/surroundMaths.R
@@ -13,37 +13,52 @@ surroundMaths <- function(x, all = FALSE, rows = NA, cols = NA, decDigits = 1){
     if ( length(decDigits) < length(rows)) decDigits <- rep(decDigits, length(rows))
     
     for (i in (1 : length(rows))){
+
       locateNA <- is.na( tempTable[ rows[i], ])
       # Now temporarily make these 0, so the  round()  can be applied
       if ( any( locateNA) ) tempTable[ rows[i], locateNA] <- 0
 
+      # Now locate other problematic entries      
+      locateOtherNonNumeric <- is.na(as.numeric( tempTable[ rows[i],]))
+      
+      # Now temporarily make these 0, so the  round()  can be applied
+      if ( any( locateOtherNonNumeric) ) tempTable[ rows[i], locateOtherNonNumeric] <- 0
       tempTable[ rows[i], ] <- paste0("$", 
-                                      format( round(x[rows[i], ], 
+                                      format( round( as.numeric(x[rows[i], ]), 
                                                     digits = decDigits[i]),
                                               nsmall = decDigits[i]),
                                       "$")
       # Now replace the NA with NA again
       if ( any( locateNA) ) tempTable[ rows[i], locateNA] <- NA
+      if ( any( locateOtherNonNumeric) ) tempTable[ rows[i], locateOtherNonNumeric] <- NA
       
     }
   }
   
   
   if ( !is.na(cols[1]) ) {
+
     if ( length(decDigits) < length(cols)) decDigits <- rep(decDigits, length(cols))
 
     for (i in (1 : length(cols))){
       locateNA <- is.na( tempTable[ , cols[i] ])
       # Now temporarily make these 0, so the  round()  can be applied
       if ( any( locateNA) ) tempTable[ locateNA, cols[i]] <- rep(0, sum(locateNA) )
+      
+      # Now locate other problematic entries      
+      locateOtherNonNumeric <- is.na(as.numeric( tempTable[ , cols[i]]))
+      
+      # Now temporarily make these 0, so the  round()  can be applied
+      if ( any( locateOtherNonNumeric) ) tempTable[ locateOtherNonNumeric, cols[i]] <- 0
 
       tempTable[, cols[i]] <- paste0("$", 
-                                     format( round(x[, cols[i]], 
+                                     format( round( as.numeric(x[, cols[i]]), 
                                                    digits = decDigits[i]), 
                                            nsmall = decDigits[i]),
                                      "$")
       # Now replace the NA with NA again
       if ( any( locateNA) ) tempTable[ locateNA, cols[i]] <- NA
+      if ( any( locateOtherNonNumeric) ) tempTable[ locateOtherNonNumeric, cols[i]] <- NA
       
     }
   }
