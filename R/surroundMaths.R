3cc01ac10fc3a4b561b6205ed1f7e6ce624c9287
Peter Dunn <pdunn2@usc.edu.au>
2023-10-26 17:23:14 +1000

    Add decDigits=NA option

--- R/surroundMaths.R
+++ R/surroundMaths.R
@@ -10,6 +10,7 @@ surroundMaths <- function(x,
   ### if  byrows = TRUE  decDigits  is applied by row.
   ### if neither  bycols  or  byrow  is give, decDigits is applied by like  bycol.
   ### decDigits should be a vector of the appropriate size (num of rows or cols), or a single number, to specify the number of decimal digits to round to.
+  ###   If decDigits is NA, then whatever is provided is used as-is. 
   ### ignore  is a logic array, the same size a x, of cells to ignore, and return as initially given in x 
   
   if ( byrows ) bycols <- FALSE
@@ -26,11 +27,16 @@ surroundMaths <- function(x,
     byrows <- FALSE
   }
   
+  doRounding <- TRUE # The defualt, when decDigits given
+  if (any(is.na(decDigits))) doRounding <- FALSE
+    
+  # Need to proceed one col (or row) at a time, to apply decDigits!
   
-  # Need to proceed one col (or row) at a time, to apply decDigits  
   if ( bycols ){
     numCols <- dimx[2]
-    if (length(decDigits) == 1) decDigits <- rep(decDigits, numCols)
+    if ( doRounding ){ 
+      if (length(decDigits) == 1) decDigits <- rep(decDigits, numCols)
+    }
     
     # Do one col at a time
     for (i in 1:numCols){
@@ -38,26 +44,42 @@ surroundMaths <- function(x,
       # This return  TRUE  if the cell is NA. So check this separately
       locateNumeric[ is.na(tempTable[, i])] <- FALSE 
       
-      tempTable[locateNumeric, i] <- paste0("$",
-                                            format( round( as.numeric(tempTable[locateNumeric, i]), 
-                                                           digits = decDigits[i]), 
-                                                    nsmall = decDigits[i]),
-                                            "$")    }
+      if ( !doRounding){
+        tempTable[locateNumeric, i] <- paste0("$",
+                                              as.numeric(tempTable[locateNumeric, i]), 
+                                              "$")    
+        
+      } else { # decDigits is gi=ven
+        tempTable[locateNumeric, i] <- paste0("$",
+                                              format( round( as.numeric(tempTable[locateNumeric, i]), 
+                                                             digits = decDigits[i]), 
+                                                      nsmall = decDigits[i]),
+                                              "$")    }
+    }
   } else {
     numRows <- dimx[1]
-    if (length(decDigits) == 1) decDigits <- rep(decDigits, numRows)
-    
+    if ( doRounding){ 
+      if (length(decDigits) == 1) decDigits <- rep(decDigits, numRows)
+    }
     # Do one row at a time
     for (i in 1:numRows){
       locateNumeric <- varhandle::check.numeric( tempTable[i, ]) # TRUE where array contains numbers
       # This return  TRUE  if the cell is NA. So check this separately
       locateNumeric[ is.na(tempTable[i, ])] <- FALSE 
       
-      tempTable[i, locateNumeric] <- paste0("$",
-                                            format( round( as.numeric(tempTable[i, locateNumeric]), 
-                                                           digits = decDigits[i]), 
-                                                    nsmall = decDigits[i]),
-                                            "$")
+      if (!doRounding){
+        tempTable[i, locateNumeric] <- paste0("$",
+                                              as.numeric(tempTable[i, locateNumeric]), 
+                                              "$")
+        
+      } else { # decZdigits is given
+        
+        tempTable[i, locateNumeric] <- paste0("$",
+                                              format( round( as.numeric(tempTable[i, locateNumeric]), 
+                                                             digits = decDigits[i]), 
+                                                      nsmall = decDigits[i]),
+                                              "$")
+      }
     }
   }
   tempTable[ignore] <- x[ignore] # Restore the  ignore  elements
