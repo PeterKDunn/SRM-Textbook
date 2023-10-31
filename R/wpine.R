6540f276e2eacd3a6e28fb280d5b093fd8309e11
Peter Dunn <pdunn2@usc.edu.au>
2023-10-31 15:34:42 +1000

    Update experimental wpine.R

--- R/wpine.R
+++ R/wpine.R
@@ -9,15 +9,23 @@
 #' }
 #' 
 
-wp <- read.csv("Data/wpine.csv")
+wp <- read.csv("Data/Wpine.csv")
+
+wp$growthRate <- with(wp,
+                       (final_height_.cm. - as.numeric(start_height..cm.)) / (2021 - start_year) )
+
 
 wp2 <- subset(wp, (soil == "Sandy") | (soil == "Loamy"))
 
+
 wp2$growthRate <- with(wp2,
-                      (final_height_.cm. - as.numeric(start_height..cm.)) / (2023 - start_year) )
+                      (final_height_.cm. - as.numeric(start_height..cm.)) / (2021 - start_year) )
+
+wp3 <- subset( wp2, 
+               (country == "Australia") & (in_pot_or_ground=="ground") )
 
 boxplot(growthRate ~ soil, 
-        data = wp2)
+        data = wp3)
 
 boxplot(growthRate ~ soil, 
         data = subset(wp2,in_pot_or_ground == "ground") )
