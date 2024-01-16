ceded0e642c10fd2f85e5ac7c40ee8bb164d05fd
Peter Dunn <pdunn2@usc.edu.au>
2024-01-16 16:18:15 +1000

    SixSteps4 now becomes SixSteps

--- R/SixSteps.R
+++ R/SixSteps.R
@@ -1,155 +1,232 @@
-
-
 # Define the six steps to use repeatedly
-SixSteps <- function( Flag = NA, 
+SixSteps <- function( Flag = 0, # 0 means to flag nothing 
                       Text = NA, 
                       Labs = "Long", 
                       Arrows = TRUE, 
+                      upDirectory = TRUE,
                       seed = NA, ...){
-  # FLAG is the box flagged for adding additional text.
+  # FLAG is the box flagged for adding additional text; a digits from 1 to 6
   # We count from the top down, so Flag=1 corresponds to Step 1: Ask
   #
   # TEXT is the extra text that it contains
   #
-  # if seed is NA (=the defaut), the items are labelled in the correct order.
+  # if seed is NA (the default), the items are labelled in the correct order.
   # Otherwise, the seed is used to generate the labels in a random order
   
-  
-  # Colours
-  col.Flag <- "slategray4"
-  col.Default <- "slategray1"  
-  
-  
-  #  blueTransparent <- rgb(0, 0, 1, alpha=0.2, maxColorValue = 1)
-  #  plot.colour <- blueTransparent
-  #  col.Default <- blueTransparent 
-  
-  
-  BoxWidth <- 1      # Width of a box
-  BoxHeight <- 0.4  # Height of a standard box (one line of text)
-  BoxGap <- 0.4     # Gap *between* boxed
-  
-  # To use  Text  in expressions, we need to use tildes rather than spaces
   Labels.Long <- c(
-    "Ask~the~question",
-    "Design~the~study",
-    "Collect~the~data",
-    "Classify*','~summarise~the~data",
-    "Analyse~the~data",
-    "Report~the~results")
+    "Ask the question",
+    "Design the study",
+    "Collect the data",
+    "Classify and summarise the data",
+    "Analyse the data",
+    "Report the results")
   
-  Labels.Short <- c("Ask",
-                    "Design",
-                    "Collect",
-                    "Describe",
-                    "Analyse",
-                    "Report")
+  Labels.Short <- c("1. Ask",
+                    "2. Design",
+                    "3. Collect",
+                    "4. Describe",
+                    "5. Analyse",
+                    "6. Report")
   
-  if ( !is.na(seed) ){
-    reSort <- sample(6)
-    Labels.Long <- Labels.Long[reSort]
-    Labels.Short <- Labels.Short[reSort]
-  }
+  ## TMP
+  imageLocations <- ifelse(upDirectory, 
+                           "../icons/SixSteps/",
+                           "icons/SixSteps/")
+  ImageAsk <- ifelse(Flag == 1, 
+                     paste0(imageLocations, "iconmonstr-help-5-240.png"),
+                     paste0(imageLocations, "iconmonstr-help-6-240.png") )
+  ImageDesign <- ifelse(Flag == 2,
+                        paste0(imageLocations, "iconmonstr-brick-3-240.png"),
+                        paste0(imageLocations, "iconmonstr-brick-4-240.png") )
+  ImageCollect <- ifelse(Flag == 3, 
+                         paste0(imageLocations, "iconmonstr-flask-21-240.png"),
+                         paste0(imageLocations, "iconmonstr-flask-1-240.png") )
+  ImageClassifySummarise <- ifelse(Flag == 4, 
+                                   paste0(imageLocations, "iconmonstr-chart-1-240.png"),
+                                   paste0(imageLocations, "iconmonstr-chart-2-240.png") )
+  ImageAnalyse <- ifelse(Flag == 5, 
+                         paste0(imageLocations, "iconmonstr-calculator-1-240.png"),
+                         paste0(imageLocations, "iconmonstr-calculator-2-240.png") )
+  ImageReport <- ifelse(Flag == 6, 
+                        paste0(imageLocations, "iconmonstr-pencil-square-filled-240.png"),
+                        paste0(imageLocations, "iconmonstr-pencil-square-lined-240.png") )
   
-  if (Labs=="Short") Labels <- Labels.Short
-  if (Labs=="Long") Labels <-  Labels.Long
-  if (Labs=="None") Labels <- rep("", 6)
-  if (Labs=="Random") Labels <- Labels.Long[ sample(1:length(Labels.Long))]
-  if (Labs=="ShortWhenWithText") {
-    Labels <- Labels.Long
-    if ( !is.na(Text) ) {
-      Labels[Flag] <- Labels.Short[Flag]
-    }
-  }
+  imageList <- c(ImageAsk,
+                 ImageDesign,
+                 ImageCollect,
+                 ImageClassifySummarise,
+                 ImageAnalyse,
+                 ImageReport)
   
-  # To use  Text  in expressions, we need to replace the spaces with tildes
-  if (is.na(Flag) ) Flag <- 0
+  
+  # SETTINGS
+  # How much to enlarge chosen image
+  enlargeFactor <- rep(0.50, 6)
+  if (Flag > 0 ) enlargeFactor[Flag] <- 0.70
+  
+  # How much to enlarge chosen box
+  enlargeBoxFactor <- rep(0.60, 6)
+  if (Flag > 0 ) enlargeBoxFactor[Flag] <- 0.80
   
 
-    # Some fooling about to make the main step in bold.
-  if ( !(Labs=="None")){ # So if we are adding labels...
-    for (j in (1:6)){    # Treat each step in the six-step process one at a time..
-      Labels[j] <- paste( "expression(bold(", Labels[j],"))")
-    }
+  boxGapHorizontal <- 1.75
+  boxGapVertical <- 2.5
+  boxWidth <- 2.5
+  boxHeight <- 2.5
+  
+  edgeBottomGap <- 1.1 * max(diff(enlargeBoxFactor)) 
+  edgeTopGap <- 1.1 * max(diff(enlargeBoxFactor))
+  edgeGap <- edgeBottomGap
+  
+  arrowToImageGap <- 0.55
+  
+  textHeight <- 1.05
+  smallTextHeight <- 0.1
+  textToImagegap <- 0.6
+  
+  plotWidth  <- (2 * edgeGap) + 
+                (3 * boxWidth ) + 
+                (2 * boxGapHorizontal)
+  plotHeight <- edgeTopGap + 
+                edgeBottomGap + 
+                (2 * boxHeight) + 
+                (2 * smallTextHeight) + 
+                (1 * boxGapVertical)
+  
+  
+  # CANVAS
+  par( mar = c(0.05, 0.05, 0.05, 0.05),
+       fin = c(2.25, 2.25) * 1.5)
+  
+  
+  ### SETUP
+  shape::emptyplot(xlim = c(0, plotWidth),
+                   ylim = c(0, plotHeight),
+                   asp = 1) # ELSE  asp=1 or requested
+  
+  pos <- array(NA, dim = c(6, 2)) # One for each of the six steps
+  
+  
+  # DEFINE box *midpoints*
+  boxX <- c(edgeGap +                          (0.5 * boxWidth),
+            edgeGap + (1 * boxGapHorizontal) + (1.5 * boxWidth),
+            edgeGap + (2 * boxGapHorizontal) + (2.5 * boxWidth),
+            edgeGap + (2 * boxGapHorizontal) + (2.5 * boxWidth),
+            edgeGap + (1 * boxGapHorizontal) + (1.5 * boxWidth),
+            edgeGap +                          (0.5 * boxWidth))
+  boxY <- c( plotHeight - edgeTopGap - (0.5 * boxWidth),
+             plotHeight - edgeTopGap - (0.5 * boxWidth),
+             plotHeight - edgeTopGap - (0.5 * boxWidth),
+             edgeBottomGap + (0.5 * boxWidth),
+             edgeBottomGap + (0.5 * boxWidth),
+             edgeBottomGap + (0.5 * boxWidth))
+
+  for (i in 1:6){
+    pos[i, ] <- c( boxX[i],
+                   boxY[i] )
   }
   
   
-  
-  
-  par( mar = c(0, 0, 0, 0) + 0.1)
-  plot( c(-BoxWidth/2, BoxWidth/2),
-        c(-BoxHeight/2, 7 * BoxHeight + 6 * BoxGap),
-        type = "n",
-        xlab = "",
-        ylab = ",",
-        axes = FALSE)
-  
-  Box.TopCorners <- (2:7) * BoxHeight + (0:5) * BoxGap  # NOTE: Box.TopCorners[1] is the lowest. So reverse
-  Box.TopCorners <- rev(Box.TopCorners)                 # NOW: Box.TopCorners[1] is the *top* box, which is Step 1
-  Box.BottomCorners <- Box.TopCorners - BoxHeight
-  
-  # Now adjust if one of the boxes is two lines of text high
-  if ( Flag > 0 ) {# If a box is flagged...
-    if (  !is.na(Text) ) {   # *AND(* there is text to put there....
-      if ( Flag == 6 ){ 
-        Box.TopCorners[ 6 ] <- Box.TopCorners[ 6]
-      } else {
-        Box.TopCorners[ (Flag + 1):6] <- Box.TopCorners[ (Flag + 1):6] - BoxHeight
-      }
-      Box.BottomCorners[ Flag:6] <- Box.BottomCorners[ Flag:6 ] - BoxHeight
-    }
-  }
-  
-  
-  DrawBox <- function(Box.TCorner, 
-                      Box.BCorner, 
-                      BoxW = BoxWidth, 
-                      which.box, 
-                      flag = FALSE, 
-                      Locations = Locations){
+  # DRAW ARROWS FIRST, then over plot with images
+  arrPos <- c(0.5, 0.5, 
+              0.45, # A bit lower, to allow for small text
+              0.5, 0.5) 
+  for (i in 1:5){ # FIVE arrow needed between SIX boxes
     
-    polygon( c(-BoxW/2, BoxW/2, BoxW/2, -BoxW/2),
-             c(Box.TCorner, Box.TCorner, Box.BCorner, Box.BCorner),
-             col = col.Default, # All boxes same colour
-             lwd = 4, # Thick borders
-             border = ifelse(flag, col.Flag, NA) ) # Thick, dark borders on flagged boxes
+    fromArrow <- pos[i, 1]
+    toArrow   <- pos[i + 1, 1]
+
+    diagram::straightarrow(from = c( pos[i, 1],
+                                     pos[i, 2] ),
+                           to   = c( pos[i + 1, 1],
+                                     pos[i + 1, 2] ),
+                           arr.pos = arrPos[i] )
   }
   
-  DrawLine <- function(Top, Bottom){
-    
-    arrows(0, Top,
-           0, Bottom,
-           length = 0.15,
-           lwd = 2,
-           angle = 15)  
-  }
+  # Draw arrow from box 6 to 1... in a lighter shade
+  diagram::straightarrow(from = c( pos[6, 1],
+                                   pos[6, 2] ),
+                         to   = c( pos[1, 1],
+                                   pos[1, 2] ),
+                         lcol = "grey",
+                         lty = 3,
+                         arr.pos = 0.55) # A bit higher, to allow for small text
+
   
+  
+  # COVER WITH WHITE BOXES
   for (i in (1:6)){
-    if (Arrows) if (i > 1 ) DrawLine( Box.BottomCorners[i - 1], Box.TopCorners[i])
+    polygon( x = c(pos[i, 1] - (boxWidth/2 * enlargeBoxFactor[i]),
+                   pos[i, 1] - (boxWidth/2 * enlargeBoxFactor[i]),
+                   pos[i, 1] + (boxWidth/2 * enlargeBoxFactor[i]),
+                   pos[i, 1] + (boxWidth/2 * enlargeBoxFactor[i])),
+             y = c(pos[i, 2] - (boxHeight/2 * enlargeBoxFactor[i]),
+                   pos[i, 2] + (boxHeight/2 * enlargeBoxFactor[i]) + 4 * smallTextHeight,
+                   pos[i, 2] + (boxHeight/2 * enlargeBoxFactor[i]) + 4 * smallTextHeight,
+                   pos[i, 2] - (boxHeight/2 * enlargeBoxFactor[i])),
+             border = "white", #ifelse( i == Flag, "black", "white"),
+             col = "white")
+  }
+  
+  # DRAW IMAGES
+  Deltax <- boxWidth / 2
+  Deltay <- boxHeight / 2 
+  
+  for (i in 1 : 6 ) { #SIX steps}
     
-    if ( i == Flag){
-      DrawBox(Box.TopCorners[i], Box.BottomCorners[i], BoxWidth, 1, TRUE)
-    } else{
-      DrawBox(Box.TopCorners[i], Box.BottomCorners[i], BoxWidth, 1, FALSE)
-    }
+    xLo <- pos[i, 1] - (Deltax * enlargeFactor[i] )
+    xHi <- pos[i, 1] + (Deltax * enlargeFactor[i] ) 
+    yLo <- pos[i, 2] - (Deltay * enlargeFactor[i] )
+    yHi <- pos[i, 2] + (Deltay * enlargeFactor[i] )
+    
+    
+    
+    plotfunctions::plot_image(img =  imageList[i],
+                              type = "png",
+                              keep.ratio = TRUE,
+                              xrange = c( xLo, xHi),
+                              yrange = c( yLo, yHi),
+                              bty = ifelse(i == Flag, 
+                                           "o",  # Keep box
+                                           "n"), # Removes box
+                              add = TRUE)
   }
   
-  # Add main text (the names of each step) into boxes
-  Text.StepsLocation <- Box.TopCorners - BoxGap/2 
-  for (i in (1:length(Labels))){
-    Text.StepsLocation.Thisone <- Text.StepsLocation[i]
-    text.col <- grey(0.3)
-    if (i == Flag ) text.col <- "black"
-    text.string <- paste("text(0, Text.StepsLocation.Thisone, ", Labels[i], ", col=text.col)")
-   # cat(text.string,"  |. <<<\n")
-    eval( parse(text = text.string))
-    #    text(0, Loc, rev(Labels)[i])
+  # Show small text
+  for (i in 1:6) {
+    text(y = pos[i, 2] + (boxHeight / 2) * enlargeBoxFactor[i] + smallTextHeight,
+         x = pos[i, 1],
+         labels = Labels.Short[i],
+         col = ifelse(i == Flag, "black", grey(0.3)),
+         cex = ifelse(i == Flag, 1, 0.9) )
   }
   
-  # Add the additional text
-  Text.AddedTextLocation <- Box.TopCorners[Flag] - BoxHeight * 1.33
-  text(0, Text.AddedTextLocation, Text)
   
+  # SHOW chosen image text
+  if (Flag > 0 ) {
+    # SHOW TEXT IN MIDDLE
+    displayText <- ifelse( is.na(Text),
+                           Labels.Short[Flag],
+                           Labels.Short[Flag] 
+                           #ifelse(Labs == "Long", 
+                            #      Labels.Long[Flag],
+                             #     Labels.Short[Flag] ),
+                           #ifelse(Labs == "Long", 
+                            #      paste0( Labels.Long[Flag], ": ", Text),
+                             #     paste0( Labels.Short[Flag], ": ", Text ))
+    )
+    diagram::textrect( mid = c( plotWidth/2, 
+                                plotHeight/2 ),
+                       radx = (plotWidth /2 ) - (boxWidth / 2) * 1.5,
+                       rady = (textHeight/2) + (edgeGap),
+                       lab = Text,
+                       box.col = gray(0.9),
+                       lcol = gray(0.9),
+                       cex = 0.95,
+                       shadow.size = 0)
+  }
+  #box()
 }
 
-#SixSteps(4)
+#SixSteps(3, "Some text here")
+
