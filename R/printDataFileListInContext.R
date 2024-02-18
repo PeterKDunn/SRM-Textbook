f1fd7634b08c08c83830fadac5d472f51349288c
Peter K Dunn <pdunn2@usc.edu.au>
2024-02-18 15:30:42 +1000

    Add links to data sets in html version

--- R/printDataFileListInContext.R
+++ R/printDataFileListInContext.R
@@ -12,7 +12,7 @@
 
 splitFiles <- matrix( c(16, 38, # col 1 is the first col, col 2 is where to stop for second col, before moving to whole new environment
                        71, 102,
-                       127, 170),
+                       127, 159),
                      nrow = 3,
                      byrow = TRUE )
 # Where to split the data file, for page 1, to create two columns. 
@@ -73,7 +73,7 @@ for (i in 1:numDataCalls){
 
 
 
-# Remove comments that may be present after the data call; eg data(FRED) # hope this data file exists!
+# Remove comments that may be present after the data call; eg data(FRED) # trust this data file exists!
 for (i in 1:numDataCalls){
   removeAfterComments <- grepl("#", 
                                dataFiles[i])
@@ -89,7 +89,10 @@ dataFiles <- dataFiles[ !duplicated(dataFiles) ]
 
 # Find actual data used:
 numDataCalls <- length(dataFiles)
-fileUsed <- array( dim = numDataCalls)
+fileUsed <- fileUsedRaw <- array( dim = numDataCalls)
+# fileUsedRaw has *just* the data fie name.
+# fileUsed also has the indications of whether an exercise, or built-in R dataset  
+
 
 for (i in 1:numDataCalls){
   # We can have two different things, depending if call in the chapter, or if listed in exercise
@@ -97,13 +100,14 @@ for (i in 1:numDataCalls){
   dataCall <- strsplit(dataFiles[i], "Rmd:")[[1]][2]
   
   
-  # Now if the first character if  [  is is in an exercise; otherwise, an actual data call
+  # Now if the first character is  [  then it is in an exercise; otherwise, an actual data call
   if ( substr( dataCall, 1, 1) == "[") {
     
     lengthDataCall <- nchar(dataCall)
     fileUsed[i] <- substr(dataCall,
                           start = 12,
                           stop = lengthDataCall - 1)
+    fileUsedRaw[i] <- fileUsed[i]
     fileUsed[i] <- paste(fileUsed[i], " (Exercise)")
   } else {
     
@@ -112,10 +116,9 @@ for (i in 1:numDataCalls){
                           start = 6,
                           stop = lengthDataCall - 1)
     fileUsed[i] <- paste0("`", fileUsed[i], "`")
+    fileUsedRaw[i] <- fileUsed[i]
   }
-  
-  
-  
+
   # Flag special cases
   if (substr(fileUsed[i], 2, 6) == "faith") { #faithful data set
     fileUsed[i] <- paste(fileUsed[i],
@@ -128,18 +131,12 @@ for (i in 1:numDataCalls){
   
   
   
-  # Now, sometimes two brackets maty occir: NHANES (Exercise) (package NHANES)
+  # Now, sometimes two brackets may occur: NHANES (Exercise) (package NHANES); replace with semicolon
   # To find, and fix
   fileUsed[i] <- gsub("\\) \\(", 
                       "; ",
                       fileUsed[i])
 }
-
-
-
-
-
-
 
 
 # Get first two characters from each entry. These are the chapter numbers
@@ -156,13 +153,45 @@ startNewChapter <- cumsum( c(1, filesPerChapter) )
 for (j in 1 : (length(filesPerChapter) ) ) { # For each chapter with data used
   startSort <- startNewChapter[j]
   stopSort <-  startNewChapter[j + 1] 
-  #cat(">>>> i:", j, "\n")
-  #print( fileUsed[startSort : stopSort] )
-  
+  if ( j == length(filesPerChapter)) stopSort <- as.numeric(chapNumbersList[ length(chapNumbersList)] )
+
+  # Sort the files within chapter
   fileUsed[startSort : stopSort] <- sort(fileUsed[startSort : stopSort])
+  fileUsedRaw[startSort : stopSort] <- sort(fileUsedRaw[startSort : stopSort])
+  
 }
 
 
+# Now add link to data file if we are creating html
+# If html format, add a link to the file in the Data directory
+if (knitr::is_html_output()) { # Add hyperlink to data: `file` (Exercise) ->  [`file`](Data/file.csv) (Exercise)
+  for (i in (1:length(fileUsed))){ # For each data file listed
+
+    backTickLocation <- unlist(gregexpr('`', fileUsed[i]))
+    bt2 <- backTickLocation[2]
+  
+    # Locate first back tick: Add  [  before
+    fileUsed[i] <- paste0("[", fileUsed[i])
+
+    # Locate second back tick: Insert  ](Data/file.csv)
+    fileUsed[i] <- paste0(substr(fileUsed[i], 
+                                 start = 1,
+                                 stop = (bt2 + 1)), # Plus 1 as we have already added leading
+                          "](Data/",
+                          substr(fileUsed[i],
+                                 start = 3,
+                                 stop = bt2), 
+                          ".csv)",
+                          substr(fileUsed[i],
+                                 start = bt2 + 2,
+                                 stop = nchar(fileUsed[i])) )
+    
+  }
+}
+  
+  
+  
+  
 
 
 
