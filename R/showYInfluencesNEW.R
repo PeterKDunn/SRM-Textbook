7e711a801039ee5c6262133304dd74d204edd5c9
Peter Dunn <pdunn2@usc.edu.au>
2024-04-02 16:11:42 +1000

    Trying new IV diagram

--- R/showYInfluencesNEW.R
+++ R/showYInfluencesNEW.R
@@ -6,14 +6,27 @@ showYInfluences <- function (showY = TRUE,
                              showConfounding = FALSE,
                              partition = NA,  # PARTITION something like c(3, 5, 2); i.e., *relative* widths
                              showLurking = FALSE){
+  
+  leftEdge <- 0
+  lowerEdge <- 0.15
+  
+  boxHeight <- 0.16
+  boxWidth <- 0.225
+  responseWidth <- 0.30
+  
+  if (showConfounding | showLurking ){
+    # Make some changes in these cases, as these are placed two beside each other
+    leftEdge <- 0.35
+    lowerEdge <- 0.30
+    responseWidth <- 0.15
+    boxWidth <- 0.20
+  }
+
   par( mar = c(0.1, 0.1, 0.1, 0.1))
   #diagram::openplotmat()
-  shape::emptyplot( xlim = c(0, 1),
-                    ylim = c(0.15, 1),
+  shape::emptyplot( xlim = c(leftEdge, 1),
+                    ylim = c(lowerEdge, 1),
                     asp = NA)
-  boxHeight <- 0.16
-  boxWidth <- 0.25
-  responseWidth <- 0.30
 
   if (showBasic){
     showChance <- FALSE
@@ -24,6 +37,25 @@ showYInfluences <- function (showY = TRUE,
     showX <- TRUE
     showY <- TRUE
   }
+  if (showConfounding){
+    showChance <- FALSE
+    showExtraneous <- TRUE
+    showLurking <- FALSE
+    showConfounding <- TRUE
+    partition <- NA
+    showX <- TRUE
+    showY <- TRUE
+  }
+  if (showLurking){
+    showChance <- FALSE
+    showExtraneous <- FALSE
+    showLurking <- TRUE
+    showConfounding <- FALSE
+    partition <- NA
+    showX <- TRUE
+    showY <- TRUE
+  }
+  
   
   if ( !all(is.na( partition) ) ){
     partition <- responseWidth * c(0, cumsum(partition) / sum(partition) )
@@ -36,18 +68,19 @@ showYInfluences <- function (showY = TRUE,
   pos[3, ] <- c(0.50, 0.9)   # Extraneous
   pos[4, ] <- c(0.85, 0.5)   # Explanatory
   
-  if ( !showBasic ) {
-    
+  if (showChance) {
     straightarrow(from = pos[2, ], # Chance
                   to = pos[1, ],   # Response
                   lty = 1, 
                   lcol = "grey",
                   lwd = 2)
+  }
+  if ( !showBasic ) {
     straightarrow(from = pos[3, ], # Extraneous 
                   to = pos[1, ],   # Response
                   lwd = ifelse( showConfounding, 2 , 2),
                   lcol = ifelse( showConfounding, "black", "grey" ),
-                  lty = ifelse( showConfounding | showLurking, 2, 1) )
+                  lty = ifelse( showLurking, 2, 1) )
   }
   
   arrowExplanatoryToResponseColour <- "black"
@@ -84,11 +117,11 @@ showYInfluences <- function (showY = TRUE,
   }
   
   if (showConfounding | showLurking ) {
-    straightarrow(from = pos[3, ], # Extraneous
+    bentarrow(from = pos[3, ], # Extraneous
                   to = pos[4, ],   # Explanatory
                   lwd = 2,
                   lcol = ifelse( showConfounding, "black", "grey" ),
-                  lty = 2)
+                  lty = ifelse( showLurking, 2, 1) )
   }
   
   textrect( pos[4,], 
@@ -99,6 +132,7 @@ showYInfluences <- function (showY = TRUE,
             box.col = ExplanatoryColour,
             lcol = ExplanatoryColour)
   if ( !all(is.na( partition) ) ) {
+    if (showChance) {
     polygon( x = c( partition[1] ,
                     partition[1] ,
                     partition[2] ,
@@ -107,7 +141,9 @@ showYInfluences <- function (showY = TRUE,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] - boxHeight/2),
+             border = "grey",
              col = ChanceColour )
+    }
     polygon( x = c( partition[2],
                     partition[2],
                     partition[3],
@@ -116,6 +152,7 @@ showYInfluences <- function (showY = TRUE,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] - boxHeight/2),
+             border = "grey",
              col = ExtraneousColour )
     polygon( x = c( partition[3],
                     partition[3],
@@ -125,35 +162,52 @@ showYInfluences <- function (showY = TRUE,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] + boxHeight/2,
                     pos[1, 2] - boxHeight/2),
+             border = "grey",
              col = ExplanatoryColour )
+    # Outside, black border
+    polygon(x = c( partition[1],
+                   partition[1],
+                   partition[4],
+                   partition[4]), 
+            y = c( pos[1, 2] - boxHeight/2,
+                   pos[1, 2] + boxHeight/2,
+                   pos[1, 2] + boxHeight/2,
+                   pos[1, 2] - boxHeight/2),
+            border = "black",
+            col = NA) # Unfilled
+    text(x = 0.5,
+         y = pos[1, 2],
+         "Response  variable")
+    text(x = pos[1, 1],
+         y = pos[1, 2] - 0.25,
+         "Total amount of variation\ncomes from different sources")
+    arrows(x0 = 0.5 - responseWidth/2,
+           x1 = 0.5 + responseWidth/2,
+           y0 = pos[1, 2] - 0.15,
+           y1 = pos[1, 2] - 0.15,
+           code = 3, # Both ends
+           length = 0.1,
+           angle = 15)
   }  else {
     textrect( pos[1,], 
-              lab = "", 
-              radx = 0.15, 
+              lab = "Response", 
+              radx = responseWidth/2, 
               rady = boxHeight/2, 
               shadow.size = 0,
               box.col = ResponseColour,
               lcol = ResponseColour)
   }
-  text(x = pos[1, 1],
-       y = pos[1, 2] - 0.25,
-       "Response variable: total amount of\nvariation comes from different sources")
-  arrows(x0 = 0.5 - responseWidth/2,
-         x1 = 0.5 + responseWidth/2,
-         y0 = pos[1, 2] - 0.15,
-         y1 = pos[1, 2] - 0.15,
-         code = 3, # Both ends
-         length = 0.1,
-         angle = 15)
          
   if (!showBasic) {
-    textrect( pos[2,], 
-              lab = "Chance",
-              radx = boxWidth/2,
-              rady = boxHeight/2,
-              shadow.size = 0,
-              lcol = ChanceColour,
-              box.col = ChanceColour )
+    if (showChance) {
+      textrect( pos[2,], 
+                lab = "Chance",
+                radx = boxWidth/2,
+                rady = boxHeight/2,
+                shadow.size = 0,
+                lcol = ChanceColour,
+                box.col = ChanceColour )
+    }
     
     ExtraneousText <- "Extraneous"
     if (showConfounding ) ExtraneousText <- "Confounding"
