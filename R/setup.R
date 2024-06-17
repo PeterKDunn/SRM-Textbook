f8e4b2bda8cffda166e0dbcdee2dd60cee1aa6cd
Peter Dunn <pdunn2@usc.edu.au>
2024-06-17 18:32:22 +1000

    Remove colour definitions

--- R/setup.R
+++ R/setup.R
@@ -24,7 +24,7 @@ library("dygraphs")
 library("slickR")
 library("qrcode")
 #library("exams")
-library("readr") # For "El Nino" 
+library("readr") # For "El Nino"
 
 
 ### Load other useful functions
@@ -35,8 +35,7 @@ source("R/SixSteps.R")
 
 
 # set global chunk options
-options(formatR.arrow = TRUE,
-        width = 90)
+options(formatR.arrow = TRUE, width = 90)
 
 # Environment defaults
 knitr::opts_chunk$set(fig.pos = "hbtp")   # Place tables HERE and so on
@@ -44,108 +43,14 @@ knitr::opts_chunk$set(fig.pos = "hbtp")   # Place tables HERE and so on
 
 # Chunk options: Options must all be on one line
 knitr::opts_chunk$set(
-  cache = FALSE, echo = FALSE, warning = FALSE, message = FALSE
+  cache = FALSE,
+  echo = FALSE,
+  warning = FALSE,
+  message = FALSE
 )
 
 # Set tables so that NA cells are not printed as NA, but left blank
 options(knitr.kable.NA = '')
-
-
-
-### COLOURS
-numCols <- 30
-someDefaultColoursTransparent <- viridis::viridis(numCols, 
-                                                  alpha = 0.4)
-colourLocation <- round(seq(1, 
-                            numCols, 
-                            length = 10) )
-# In order: NA, Explanatory, Chance, Design, Extraneous, NA
-ResponseColourTransparent <- someDefaultColoursTransparent[ colourLocation[3] ]
-ChanceColourTransparent <- someDefaultColoursTransparent[ colourLocation[4] ]
-ExplanatoryColourTransparent <- someDefaultColoursTransparent[ colourLocation[5] ]
-DesignColourTransparent <- someDefaultColoursTransparent[ colourLocation[6] ]
-ExtraneousColourTransparent <- someDefaultColoursTransparent[ colourLocation[7] ]
-GroupColourTransparent <- someDefaultColoursTransparent[ colourLocation[8] ]
-IndividualColourTransparent <- someDefaultColoursTransparent[ colourLocation[9] ]
-BlockColourTransparent <- someDefaultColoursTransparent[ colourLocation[10] ]
-
-# TEST COLOURS:
-# 
-# plot( x = c(0, 1.6),
-#       y = c(0, 1) )
-# polygon( x = c(0, 0, 0.2, 0.2),
-#          y = c(0, 1, 1, 0),
-#          col = ResponseColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 0.2,
-#          y = c(0, 1, 1, 0),
-#          col = ChanceColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 0.4,
-#          y = c(0, 1, 1, 0),
-#          col = ExplanatoryColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 0.6,
-#          y = c(0, 1, 1, 0),
-#          col = DesignColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 0.8,
-#          y = c(0, 1, 1, 0),
-#          col = GroupColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 1.0,
-#          y = c(0, 1, 1, 0),
-#          col = IndividualColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 1.2,
-#          y = c(0, 1, 1, 0),
-#          col = ExtraneousColourTransparent)
-# polygon( x = c(0, 0, 0.2, 0.2) + 1.4,
-#          y = c(0, 1, 1, 0),
-#          col = BlockColourTransparent)
-
-
-# Convert to equiv solid, manually...
-ResponseColour <- rgb(187, 188, 208, 
-                      maxColorValue = 256)
-ChanceColour <- rgb(187, 183, 205, 
-                    maxColorValue = 256)
-ExplanatoryColour <- rgb(186, 195, 210, 
-                         maxColorValue = 256)
-DesignColour <- rgb(185, 206, 211, 
-                    maxColorValue = 256)
-ExtraneousColour <- rgb(198, 230, 203, 
-                        maxColorValue = 256)
-GroupColour <- rgb(223, 240, 190, ### FIX!
-                   maxColorValue = 256)
-IndividualColour <- rgb(187, 217, 210, ### FIX!
-                        maxColorValue = 256)
-BlockColour <- rgb(253, 245, 187, ### FIX!
-                   maxColorValue = 256)
-
-
-### OTHER COLOURS
-blueSolid <- rgb(214, 215, 252, 
-                 maxColorValue = 256)
-plotSolid <- blueSolid
-
-plotDark <- rgb(0, 0, 1,
-                maxColorValue = 1)
-
-blueTransparent <- rgb(0, 0, 1, 
-                       alpha = 0.2, 
-                       maxColorValue = 1)
-blueTransparent0 <- rgb(0, 0, 1, 
-                        alpha = 0.8, 
-                        maxColorValue = 1)
-blueTransparent2 <- rgb(0, 0, 0.25, 
-                        alpha = 0.2, 
-                        maxColorValue = 1)
-
-### MOVED LATER, as t is too early to work here (I guess, the output not yet set)
-plot.colour <-  grey(0.75) 
-plot.colour0 <-  grey(0.5) 
-plot.colour2 <-  grey(0.75) 
-
-if ( knitr::is_html_output()) {
-  plot.colour <- blueTransparent
-  plot.colour0 <- blueTransparent0
-  plot.colour2 <- blueTransparent2
-}
 
 
 
@@ -154,26 +59,26 @@ if ( knitr::is_html_output()) {
 ### FUNCTIONS ###
 
 ###
-findStdError <- function(x, na.rm = TRUE){
-  sd(x, na.rm = TRUE) / sqrt( length(x) - sum(is.na(x)) )
+findStdError <- function(x, na.rm = TRUE) {
+  sd(x, na.rm = TRUE) / sqrt(length(x) - sum(is.na(x)))
 }
 
 ###
-realLength <- function(x, na.rm = TRUE){
+realLength <- function(x, na.rm = TRUE) {
   length(x) - sum(is.na(x))
 }
 
 ###
 ### From: https://stackoverflow.com/questions/8047668/transparent-equivalent-of-given-color
-makeTransparent <- function(someColor, alpha = 100){
-  newColor <- col2rgb( someColor )
-  apply(newColor, 
-        2, 
-        function(curcoldata){ rgb(red = curcoldata[1], 
-                                  green = curcoldata[2],
-                                  blue = curcoldata[3],
-                                  alpha = alpha, 
-                                  maxColorValue = 255)
-        }
-  )
+makeTransparent <- function(someColor, alpha = 100) {
+  newColor <- col2rgb(someColor)
+  apply(newColor, 2, function(curcoldata) {
+    rgb(
+      red = curcoldata[1],
+      green = curcoldata[2],
+      blue = curcoldata[3],
+      alpha = alpha,
+      maxColorValue = 255
+    )
+  })
 }
