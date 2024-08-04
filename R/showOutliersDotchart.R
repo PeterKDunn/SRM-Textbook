49b7df38bfa5253410f8644b5ce80308f6667525
Peter K Dunn <pdunn2@usc.edu.au>
2024-08-05 08:41:22 +1000

    Add left-side action for finding outliers

--- R/showOutliersDotchart.R
+++ R/showOutliersDotchart.R
@@ -1,11 +1,11 @@
 transparentGrey <- makeTransparent(grey(0.90))
 angle <- 15
-
+set.seed(77201)
 ######################################
 
 
 
-par( mar = c(4.5, 2.0, 4, 2.0) + 0.1)
+par( mar = c(4.5, 1.0, 4, 1.0) + 0.1)
 
 data(Perm)
 Perm.quantiles <- quantile(Perm$Perm)
@@ -13,8 +13,10 @@ Perm.quantiles <- quantile(Perm$Perm)
 
 ### Sort out the outliers
 x <- Perm$Perm
-xsort <- sort( x , decreasing = TRUE)  
-b.out <- boxplot(x, plot = FALSE)
+xsort <- sort( x , 
+               decreasing = TRUE)  
+b.out <- boxplot(x, 
+                 plot = FALSE)
 
 b.out.noextreme <- b.out
 b.out.noextreme$n <- b.out.noextreme$n - 2
@@ -31,6 +33,8 @@ qx <- quantile(x)
 iqrx <- qx[4] - qx[2]
 mild <- qx[4] + 1.5 * iqrx
 extreme <- qx[4] + 3 * iqrx
+mildLO <- qx[2] - 1.5 * iqrx
+extremeLO <- qx[2] - 3 * iqrx
 
 # FIND extreme obs
 extreme.obs <-  sort(b.out$out)[2:3]
@@ -54,19 +58,20 @@ stripchart( Perm$Perm,
             #col = "grey",
             axes = FALSE,
             ylim = c(-0.5, 2.2),
-            xlim = c(0, 175),
+            xlim = c(-85, 175),
             xlab = "Permeability (in s)")
 axis(side = 1, 
-     at = c(0, Perm.quantiles[2], 50, Perm.quantiles[4], 100, 150, 175),
-     cex = 0.95,
-     labels =  c(0,
-                 expression(italic(Q)[1]), 
-                 "",
-                 expression(italic(Q)[3]),
-                 100,
-                 150,
-                 175)
-     )
+     at = seq( -75, 175, by = 25),
+     cex = 0.95)
+
+text(x = Perm.quantiles[2], 
+     y = 2.2, 
+     xpd = TRUE,
+     label = expression(italic(Q)[1]))
+text(x = Perm.quantiles[4], 
+     y = 2.2, 
+     xpd = TRUE,
+     label = expression(italic(Q)[3]))
 
 lines( x = c( Perm.quantiles[2],
               Perm.quantiles[2] ),
@@ -86,30 +91,31 @@ lines( x = c( Perm.quantiles[4],
 ### Add IQR
 arrows(x0 = Perm.quantiles[2],
        x1 = Perm.quantiles[4],
-       y0 = 1.75,
-       y1 = 1.75,
+       y0 = 1.65,
+       y1 = 1.65,
        code = 3,
        lwd = 2,
        angle = angle,
        length = 0.1)
 text(labels = "IQR",
-     y = 1.75,
+     y = 1.65,
      cex = 1.25,
      x = mean( Perm.quantiles[c(2, 4)]),
      pos = 3)
 
+
 ### MILD OUTLIERS
 arrows(x0 = Perm.quantiles[4],
        x1 = mild,
-       y0 = 0.7,
-       y1 = 0.7,
+       y0 = 0.65,
+       y1 = 0.65,
        code = 3,
        lwd = 2,
        angle = angle,
        length = 0.1)
 text( x = mean( c(Perm.quantiles[4], 
                   mild)),
-      y = 0.7,
+      y = 0.65,
       pos = 1,
       labels = expression(1.5~x~IQR))
 
@@ -154,5 +160,51 @@ arrows(x0 = mean( extreme.obs ) + c(-4, 4),
        y1 = 1.15,
        angle = 15,
        length = 0.1)
+
+
+### MILD OUTLIERS: Lower end
+arrows(x0 = Perm.quantiles[2],
+       x1 = mildLO,
+       y0 = 0.65,
+       y1 = 0.65,
+       code = 3,
+       lwd = 2,
+       angle = angle,
+       length = 0.1)
+text( x = mean( c(Perm.quantiles[2], 
+                  mildLO)),
+      y = 0.65,
+      pos = 1,
+      labels = expression(1.5~x~IQR))
+
+polygon( y = c(-5, 10, 10, -5),
+         x = c(mildLO, mildLO, extremeLO, extremeLO),
+         border = NA, # No border
+         col = transparentGrey )
+abline( v = c(mildLO, extremeLO),
+        lwd = 2,
+        col = "grey")
+text(x = mean( c(mildLO, extremeLO) ),
+     y = 1.75,
+     labels = "No mild\noutliers")
+
+### EXTREME OUTLIERS
+arrows(x0 = Perm.quantiles[2],
+       x1 = extremeLO,
+       y0 = 0,
+       y1 = 0,
+       lwd = 2,
+       code = 3,
+       angle = angle,
+       length = 0.1)
+text( x = mean( c(Perm.quantiles[2], 
+                  extremeLO)),
+      y = 0,
+      pos = 1,
+      labels = expression(3~x~IQR~"  "))
+text(x = -70,
+     y = 1.75,
+     labels = "No extreme\noutliers")
+
 
 
