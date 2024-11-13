49f59d8e681e818615ef60d5e2effd2dd10505f5
Peter Dunn <pdunn2@usc.edu.au>
2024-11-13 18:54:07 +1000

    Sort separately by exercise, non-exercise; add small space between exercise, non-exercise

--- R/printDataFileListInContext.R
+++ R/printDataFileListInContext.R
@@ -116,7 +116,8 @@ cleanDataFileCalls  <- function(dataFiles){
 
 
 ######################################################
-classifyDataMentionsTypes <- function(dataFiles){
+classifyDataMentionsTypes <- function(dataFiles, 
+                                      inLaTeX){
   
   # Find whether mentioned in the chapter, or in an exercise
   # NOT sure I HAVE THIS CORRRECT!!!
@@ -162,23 +163,22 @@ classifyDataMentionsTypes <- function(dataFiles){
     
     # Then: one approach is to look for the string  ### Exercise  and replace this with 
     
-    # Maybe also: If we have a  data(Dataset)###Exercise  call, but no associated [*Dataset*: `Dataset`]  we can flag and/or suggest adding it
+    # Maybe also: If we have a  data(Dataset) ### Exercise  call, but no associated [*Dataset*: `Dataset`]  we can flag and/or suggest adding it
     
     if (grepl('###Exercise', dataCall)) { # If we see the string  ###Exercise...
       
       dFileStem <- unlist( strsplit( dataCall, 
-                                     "###")) # Split where ### is found: Returns somethiungs like  data(fred); we just want the  fred, so split again
+                                     "###")) # Split where ### is found: Returns something   like  data(fred); we just want the  fred, so split again
       dFileStem <- unlist( strsplit( dFileStem, 
                                      "\\("))[2] # Split where  (  is found. This returns  fred).
       dFileStem <- substr( dFileStem, 
                            start = 1,
                            stop = nchar(dFileStem) - 1 )
       
-      # So now rename it to look like an exercise
+      # So now rename it to look like an exercise in what follows
       dataCall <- paste0("[*Dataset*:`",
                          dFileStem[1],
                          "`]")
-      
     }
     
     
@@ -206,12 +206,22 @@ classifyDataMentionsTypes <- function(dataFiles){
     
     # Flag special cases
     if (substr(fileUsed[i], 2, 6) == "faith") { #faithful data set
-      fileUsed[i] <- paste(fileUsed[i],
-                           "(in **R**)" )
+      if (inLaTeX) {
+        fileUsed[i] <- paste(fileUsed[i],
+                             "(in \textbf{R})" )        
+      } else {
+        fileUsed[i] <- paste(fileUsed[i],
+                             "(in **R**)" )
+      }
     }
-    if (substr(fileUsed[i], 2, 6) == "NHANE") { #faithful data set
-      fileUsed[i] <- paste(fileUsed[i],
-                           "(in **NHANES** package)" )
+    if (substr(fileUsed[i], 2, 6) == "NHANE") { # NHANES data set
+      if (inLaTeX) {
+        fileUsed[i] <- paste(fileUsed[i],
+                             "(in \\textbf{NHANES} package)" )
+      } else {
+        fileUsed[i] <- paste(fileUsed[i],
+                             "(in **NHANES** package)" )
+      }
     }
     
     
@@ -246,13 +256,47 @@ sortDataFilesByChapter <- function(dataFiles, chapterNumbers){
   names(listDataFilesByChapter) <- paste("Chapter ", 
                                          unique(chapterNumbers), sep = " ")
   
-  # Alphabetical order
-  listDataFilesByChapter <- lapply(listDataFilesByChapter, sort)
-  #cat("--------------------------------------------------------------------- RESULT\n")
-  #print(listDataFilesByChapter)
+  # Now also sort, to have Exercise *after* the in-chapter exercises
+  numberChapters <- length( listDataFilesByChapter) # The length of the list listDataFilesByChapter
+  for (i in 1:numberChapters){ # For each chapter:
+    #cat("----\n CHAPTER", i, "\n")
+    thisChaptersList <- listDataFilesByChapter[[i]]
+    #cat("* The file", thisChaptersList, "\n")
+
+    sortByExercise <- grepl("\\(Exercise", 
+                            thisChaptersList) # Find which elements have "(Exercise" in them)
+    out <- sort( sortByExercise,
+                 index.return = TRUE)$ix
+    listDataFilesByChapter[[i]] <- listDataFilesByChapter[[i]][ out ]
+    
+  # Each list element of  listDataFilesByChapter  now organised by non-Exercise data sets first, 
+  # then all the exercise data set
+  # So we separate the two (Exercise; non-Exercise) and sort separately, then rejoin
+
+    thisChaptersList <- listDataFilesByChapter[[i]]
+    #cat("+ The file", thisChaptersList, "\n")
+    isExercise <- grepl("Exercise", 
+                        thisChaptersList)
+    if ( sum(isExercise) > 0 ){
+      ExerciseOnly <- order( thisChaptersList[ isExercise ])
+    } else {
+      ExerciseOnly <- NULL
+    }
+    if ( sum(!isExercise) > 0 ){
+      NonExerciseOnly <- order( thisChaptersList[ !isExercise ])
+    } else {
+      NonExerciseOnly <- NULL
+    }
+    # Recombine, in order
+    #cat("\n@ The file", listDataFilesByChapter[[i]] , "\n")
+    #cat(c( NonExerciseOnly, 
+    #       length(NonExerciseOnly) + ExerciseOnly) )
+    listDataFilesByChapter[[i]] <- listDataFilesByChapter[[i]][c( NonExerciseOnly, 
+                                                                  length(NonExerciseOnly) + ExerciseOnly) ]
+    #cat("\n- The file", listDataFilesByChapter[[i]] , "\n")
+  }
   
-  
-  return(listDataFilesByChapter)
+   return(listDataFilesByChapter)
 }
 
 
@@ -260,79 +304,64 @@ sortDataFilesByChapter <- function(dataFiles, chapterNumbers){
 
 
 ######################################################
-addHyperLinks <- function(fileNames){
+prepareFileName <- function(fileName, inLaTeX){
   
-  # fileNames is a *list*, with entries like "fileNames$Chap07" containing all the data fies for that chapter.
+  # fileNames is a single file name, with entries like "`NMiner` (Exercise)" (note the back-ticks!)
+  # Only the fiule bame shoud be in tt font, not the stuff after it!
+
+  backTickLocation <- unlist(gregexpr('`', 
+                                      fileName))
+  bt1 <- backTickLocation[1]
+  bt2 <- backTickLocation[2]
   
-  # If html format, add a link to the file in the Data directory
-  for (i in (1:length(fileNames))){ # For each data file listed
-    
-    backTickLocation <- unlist(gregexpr('`', fileNames[i]))
-    bt2 <- backTickLocation[2]
+  if (inLaTeX) {
+    fileNameTmp <- fileName
+    # Locate back-ticks
+
+    fileNameTmp <- paste0("\\texttt{", 
+                          substr(fileNameTmp, 
+                                 start = bt1 + 1,
+                                 stop = bt2 - 1),
+                          "}")
+  # Now add anything that followed the last back tick
+    fileName <- paste0( fileNameTmp,
+                        substr(fileName,
+                               start = bt2 + 1,
+                               stop = nchar(fileName))
+    )
+  } else {
     
     # Locate first back tick: Add  [  before
-    fileUsed[i] <- paste0("[", fileNames[i])
+    fileUsed[i] <- paste0("[", fileName)
     
     # Locate second back tick: Insert  ](Data/file.csv)
-    fileUsed[i] <- paste0(substr(fileNames[i], 
+    fileUsed[i] <- paste0(substr(fileName, 
                                  start = 1,
                                  stop = (bt2 + 1)), # Plus 1 as we have already added leading
                           "](Data/",
-                          substr(fileNames[i],
+                          substr(fileName,
                                  start = 3,
                                  stop = bt2), 
                           ".csv)",
-                          substr(fileNames[i],
+                          substr(fileName,
                                  start = bt2 + 2,
-                                 stop = nchar(fileNames[i])) )
+                                 stop = nchar(fileName)) )
     
   }
   
-  return(fileNames)
+  return(fileName)
   
 }
 
 
 ######################################################
 writeDataFileList <- function(fileNames, 
-                              splitFiles = NULL, # Where to break columns
-                              addLinks = FALSE){ # In HTML only, add links 
-  
-  if (is.null(splitFiles)) {  
-    splitFiles <- c(21, 
-                    46, # End of page 1
-                    78, 
-                    111,# End of page 2
-                    132, 
-                    1540)
-  }
-  # Where to split the data file, for page 1, to create two columns. 
-  # So if e.g., split = 20, we will have 20 files listed, then move to col 2, then another 20 files listed.
-  #
-  # them move on to a new column environment
-  # 
-  # OR: give HOW MANY are in each col
+                              inLaTeX = FALSE,
+                              addLinks = !inLaTeX){ # In HTML only, add links 
   
   # Prep: define useful bits
   numberOfDataFiles <- sum(unlist(lapply(fileNames, length)))
   
-  splitLineNumbers <- c(1,
-                        splitFiles,
-                        numberOfDataFiles)
-  lengthSplitLineNumbers <- length(splitLineNumbers)
-  
-  startLeftColumns <- splitLineNumbers[ seq(1, lengthSplitLineNumbers, by = 2)]
-  startRightColumns <- splitLineNumbers[ 1 + seq(1, lengthSplitLineNumbers, by = 2)]
-  
-  endLeftColumns <- startRightColumns - 1
-  endRightColumns <- c(startLeftColumns - 1, 
-                       numberOfDataFiles)
-  startRightColumns <- startRightColumns[1 : (length(startRightColumns) - 1)]
-  
-  startPage <- startLeftColumns 
-  endPage <- endRightColumns
-  
-  numberOfPages <- length(splitFiles) / 2
   numberOfChapters <- length(fileNames)
   
   numberFilesPerChapter <- unlist(lapply(fileNames, 
@@ -344,117 +373,139 @@ writeDataFileList <- function(fileNames,
   # Get chapter numbers for each data file
   chapterNumbers <- names(unlist(lapply(fileNames, 
                                         length)))
-  #cat("chapterNumbers =", chapterNumbers, "\n")
   chapterNumbersForEachDataFile <- rep(chapterNumbers, 
                                        unlist(lapply(fileNames, length)))
-  #cat("A: chapterNumbersForEachDataFile = ", chapterNumbersForEachDataFile, "\n")
-  #chapterNumbersForEachDataFile <- substr(chapterNumbersForEachDataFile,
-  #                                        start = 5,
-  #                                        stop = 6)
-  #cat("B: chapterNumbersForEachDataFile = ", chapterNumbersForEachDataFile, "\n")
+
+  # Start two-column mode in LaTeX only:
+  # Only need the two columns for LaTeX; it looks silly in HTML
+  if ( inLaTeX ) {
+    # START PAGE with column set-up info for the whole PAGE
+    cat("\\begin{multicols}{2}\\raggedcolumns\n")
+  }    
   
-  # Remove leading zeros
-  leadingZeros <- substr(chapterNumbersForEachDataFile, 1, 1) == "0"
-  for (i in 1:length(leadingZeros)){
-    if (leadingZeros[i]){
-      substr(chapterNumbersForEachDataFile[i],
-             start = 1,
-             stop = 1) <- " " # Replace with a space
-    }
-  }
-  
-  #  cat("Number of data files", numberOfDataFiles, "\n")
-  for (i in 1:numberOfDataFiles){
+  for (i in 1:length(numberFilesPerChapter) ) { # For each chapter:
     
-    # Only need the two columns for LaTeX; it looks silly in HTML
-    if ( !addLinks ) {
-      if ( i %in% startPage){
-        # START PAGE with column set-up info for the whole PAGE
-        cat(':::::: {.cols data-latex=\"[T]\"}\n')
-        cat("::: {.col data-latex=\"{0.48\\textwidth}\"}\n")
-      }
-    }    
-    
-    if (i %in% startChapter){
-      cat("\n\\medskip\\goodbreak\n**",
-          sub("^0+", "", chapterNumbersForEachDataFile[i]), # Remove any leading zeros
-          "**\n\n",
-          sep = "")
+    if ( i > 1 ){ # No need to close if we haven't had a minipage start yet
+      # After all the files are listed, close the  minipage  in LaTeX before starting on the next chapter's list
+      cat("\\end{itemize}",
+          "\\end{minipage}\n",
+          "\\medskip")
     }
+    
+    cat("\n\n\\begin{minipage}{\\textwidth}\n")
+    
+    # Start bold
+    if( inLaTeX ){
+      cat("\\textbf{")
+    } else {
+      cat("**")
+    }
+    # Print chapter number in this bolding environment, after removing any leading zeros on the chapter number
+    cat( sub("^0+",  # Remove any leading zeros
+             "", 
+             chapterNumbers[i]) 
+         )
+    # End bold
+    if (inLaTeX) {
+      cat("}\n")
+    } else {
+      cat("**\n")
+    }
+    
+    # Begin the itemize environment
+    if (inLaTeX) cat("\\begin{itemize}\\tightlist\n")
     
     # PRINT DATA FILE INFO
     # ADD HYPERLINKS if requested (i.e., HTML)
-    if (addLinks) { 
-      fileNameWithLinks <- unlist(fileNames)[i]
-      
-      # Add hyperlink to data: `file` (Exercise) ->  [`file`](Data/file.csv) (Exercise)
-      backTickLocation <- unlist(gregexpr('`', 
-                                          fileNameWithLinks))
-      bt2 <- backTickLocation[2]
-      
-      # Locate first back tick: Add  [  before
-      fileNameWithLinks <- paste0("[", fileNameWithLinks)
-      
-      # Locate second back tick: Insert  ](Data/file.csv)
-      fileNameWithLinks <- paste0(substr(fileNameWithLinks,
-                                         start = 1,
-                                         stop = (bt2 + 1)), # Plus 1 as we have already added leading
-                                  "](Data/",
-                                  substr(fileNameWithLinks,
-                                         start = 3,
-                                         stop = bt2), 
-                                  ".csv)",
-                                  substr(fileNameWithLinks,
-                                         start = bt2 + 2,
-                                         stop = nchar(fileNameWithLinks)) )
-      # Print 
-      cat( "* ",
-           fileNameWithLinks,
-           "\n") 
-      
+    if ( i > 1 ) {
+      startHere <- sum(numberFilesPerChapter[1 : (i- 1)] ) + 1 
+      endHere <- sum(numberFilesPerChapter[1:i] )
     } else {
-      # Just print 
-      cat( "* ",
-           unlist(fileNames)[i],
-           "\n") 
+      startHere <- 1
+      endHere <- numberFilesPerChapter[1]
     }
-    
-    # Only need the two columns for LaTeX; it looks silly in HTML
-    if ( !addLinks ) {
-      if ( i %in% startRightColumns){
-        cat('::: \n')
-        cat('::: {.col data-latex=\"{0.03\\textwidth}\"} \n')
-        cat('\\ \n') 
-        ## <!-- an empty Div (with a white space), serving as
-        ## a column separator -->
-        cat(':::\n')
-        cat('::: {.col data-latex=\"{0.48\\textwidth}\"}\n')
-      }
+
+    isPreviousExercise <- TRUE
+    isCurrentExercise <- FALSE
+    # Now print each file name
+    for (j in startHere : endHere) { # For each data file in the current chapter:
+
+      # detect if it contains an "Exercise" or not
+      isPreviousExercise <- isCurrentExercise
+      isCurrentExercise <- grepl("Exercise",
+                                 unlist(fileNames)[j])
+      isStartOfExercises <- isCurrentExercise & (!isPreviousExercise)
+      
+      if (inLaTeX) {  
+          # Just print 
+          if (isStartOfExercises) cat("\n\\medskip\n") # Little space before exercises start
+          
+          cat( "\\item\n ",
+               prepareFileName( unlist(fileNames)[j],
+                                inLaTeX = TRUE),
+               "\n") 
+        } else { # HTML needs hyperlinks added, so complicated
+          cat( "* \n",
+               prepareFileName( unlist(fileNames)[j],
+                                inLaTeS = FALSE),
+               "\n") 
+          
+        #   
+        #   
+        # fileNameWithLinks <- unlist(fileNames)[i]
+        # 
+        # # Add hyperlink to data: `file` (Exercise) ->  [`file`](Data/file.csv) (Exercise)
+        # backTickLocation <- unlist(gregexpr('`', 
+        #                                     fileNameWithLinks))
+        # bt2 <- backTickLocation[2]
+        # 
+        # # Locate first back tick: Add  [  before
+        # fileNameWithLinks <- paste0("[", fileNameWithLinks)
+        # 
+        # # Locate second back tick: Insert  ](Data/file.csv)
+        # fileNameWithLinks <- paste0(substr(fileNameWithLinks,
+        #                                    start = 1,
+        #                                    stop = (bt2 + 1)), # Plus 1 as we have already added leading
+        #                             "](Data/",
+        #                             substr(fileNameWithLinks,
+        #                                    start = 3,
+        #                                    stop = bt2), 
+        #                             ".csv)",
+        #                             substr(fileNameWithLinks,
+        #                                    start = bt2 + 2,
+        #                                    stop = nchar(fileNameWithLinks)) )
+        # # Print 
+        # cat( "* ",
+        #      fileNameWithLinks,
+        #      "\n") 
+        
+      } 
     }
-    
-    # Only need the two columns for LateX; it looks silly in HTML
-    if ( !addLinks ) {
-      if (i %in% endRightColumns ){ 
-        cat(':::\n')
-        cat('::::::\n')
-      }
-    }
-    
-    
   }
+  # Close two-column mode in LaTeX only
+  if ( inLaTeX ) {
+    cat("\\end{itemize}\n",
+        "\\end{minipage}\n",
+        "\\end{multicols}\n")
+  } 
+  
 }
+
+
 
 ######################################################
 ### DO THE EXTRACTION ###
 dFiles <- findDataFileMentions() # A list text of the *lines* that contains mentions of data files
 dFiles1 <- cleanDataFileCalls(dFiles) # Remove trailing text and comments
-out <- classifyDataMentionsTypes(dFiles1) # Find whether mentioned in the chapter, or in an exercise
+out <- classifyDataMentionsTypes(dFiles1,
+                                 inLaTeX = TRUE ) # is_latex_output() # Find whether mentioned in the chapter, or in an exercise
 chapNum <- out$chapNumbers
 dFiles2 <- out$fileNames
-dFiles3 <- sortDataFilesByChapter(dFiles2, chapNum) # Create a  list()  with an entry for each chapter, giving a vector of data file names 
+dFiles3 <- sortDataFilesByChapter(dFiles2, 
+                                  chapNum) # Create a  list()  with an entry for each chapter, giving a vector of data file names 
 
 writeDataFileList(dFiles3, # WRITE
-                  splitFiles, 
-                  addLinks = is_html_output() )
+                  inLaTeX  = TRUE, #is_latex_output(),
+                  addLinks = FALSE) # is_html_output() )
 ######################################################
 
