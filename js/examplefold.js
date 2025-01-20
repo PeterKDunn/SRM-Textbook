7cfc613d156c858b2f28e5448e41f77eaf1ae421
Peter Dunn <pdunn2@usc.edu.au>
2025-01-20 11:13:16 +1000

    "Example" -> "Extra example"

--- js/examplefold.js
+++ js/examplefold.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $examplefolds.prepend("<button class=\"example-fold-btn\" style=\"border-style=solid; border-color: black; padding: 12px 25px; font-size: larger; background-color=LightSlateGrey\">Example: Show</button>");  // add a button
   $(".example-fold-blck").toggle();  // fold all blocks
   $(".example-fold-btn").on("click", function() {  // add onClick event
-    $(this).text($(this).text() === "Example: Hide" ? "Example: Show" : "Example: Hide");  // if the text equals "Example: Hide", change it to "Example: Show"or else to "Example: Hide" 
+    $(this).text($(this).text() === "Extra example: Hide" ? "Extra example: Show" : "Extra example: Hide");  // if the text equals "Example: Hide", change it to "Example: Show"or else to "Example: Hide" 
     $(this).next(".example-fold-blck").toggle("linear");  // "swing" is the default easing function. This can be further customized in its speed or the overall animation itself.
   })
 });
